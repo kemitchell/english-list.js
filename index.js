61f402825be663a4a73099ab47ad0a1122b328c0
Kyle E. Mitchell <kyle@kemitchell.com>
2017-08-10 15:19:36 -0700

    Use JavaScript Standard Style

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
-module.exports = function(conjunction, array, serialComma) {
-  if (!Array.isArray(array) || !(conjunction.toString)) {
-    throw new TypeError();
+module.exports = function (conjunction, array, serialComma) {
+  if (!Array.isArray(array) || !conjunction.toString) {
+    throw new TypeError()
   }
-  conjunction = conjunction.toString();
-  serialComma = serialComma === undefined ? true : !!serialComma;
-  var length = array.length;
+  conjunction = conjunction.toString()
+  serialComma = serialComma === undefined ? true : !!serialComma
+  var length = array.length
   if (length === 0) {
-    throw new Error('Cannot create an English list of an empty array');
+    throw new Error('Cannot create an English list of an empty array')
   } else if (length === 1) {
-    return array[0].toString();
+    return array[0].toString()
   } else if (length === 2) {
     return (
       array[0].toString() +
-      ' ' + conjunction + ' ' +
+      ' ' +
+      conjunction +
+      ' ' +
       array[1].toString()
-    );
+    )
   } else {
-    var strings = array.map(function(element) {
-      return element.toString();
-    });
-    var front = strings.slice(0, length - 1);
-    var last = strings[length - 1];
+    var strings = array.map(function (element) {
+      return element.toString()
+    })
+    var front = strings.slice(0, length - 1)
+    var last = strings[length - 1]
     return (
       front.join(', ') +
       (serialComma ? ', ' : ' ') +
-      conjunction + ' ' +
+      conjunction +
+      ' ' +
       last
-    );
+    )
   }
-};
+}
 
-module.exports.version = '1.0.0-prerelease-1';
+module.exports.version = '1.0.0-prerelease-1'
